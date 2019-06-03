b1451019a9326a67421ebd32daaa4811431d0cf8
qiuyexiang <51324021+qiuyexiang@users.noreply.github.com>
2019-06-03 20:33:56 +0800

    Update HellowController.java

--- src/main/java/comht/springboot/controller/HellowController.java
+++ src/main/java/comht/springboot/controller/HellowController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HellowController {
     @GetMapping("/abc")
    public String hello(Model model){
-        model.addAttribute("mag","你好");
+        model.addAttribute("mag","你好徐家斌最帅！！！嘿嘿嘿");
        return "success";
 }
 }
